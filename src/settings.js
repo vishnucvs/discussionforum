28e60d483fbec1e0fd0170bd80d8d899e974f5dc
vishnucvs <64590388+vishnucvs@users.noreply.github.com>
2020-06-03 10:13:07 +0530

    Commit 1
    first commit

---
+++ src/settings.js
@@ -0,0 +1,9 @@
+// let DEBUG = false;
+// let HOST_URL = "https://justdjango-chat.herokuapp.com";
+// let SOCKET_URL = "wss://justdjango-chat.herokuapp.com";
+// if (DEBUG) {
+let HOST_URL = "http://127.0.0.1:8000";
+let SOCKET_URL = "ws://127.0.0.1:8000";
+//}
+
+export { HOST_URL, SOCKET_URL };
